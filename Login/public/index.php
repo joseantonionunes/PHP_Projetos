7b6d37523f7cf5688178e2c23306ebad171868b7
joseantonionunes <joseantonionunes2018@gmail.com>
2024-12-04 15:16:49 -0300

    login

---
+++ Login/public/index.php
@@ -0,0 +1,38 @@
+<?php
+
+// INICIALIZA A SESSÃO
+session_start();
+
+//define uma constante de controle
+define('CONTROL', true);
+
+//verifica se o usuário está logado
+$usuario_logado = $_SESSION['usuario'] ?? null;
+
+//verificar qual é a rota da URL
+if(empty($usuario_logado)){
+    $rota = 'login';
+} else{
+    $rota = $_GET['rota'] ?? 'home';
+}
+
+// se o usuário está logado, mas a rota é login, então redirecina para home
+if (!empty($usuario_logado) && $rota == 'login') {
+    $rota = 'home';
+} 
+
+// analisa as rotas
+$rotas = [
+    'login' => 'login.php',
+    'home' => 'home.php',
+    'page1' => 'page1.php',
+    'page2' => 'page2.php',
+    'page3' => 'page3.php',
+    'logout' => 'logout.php'
+];
+
+if(!array_key_exists($rota, $rotas)){
+    die('Acesso negado!');
+}
+
+require_once $rotas[$rota];
---
+++ Login/public/login.php
@@ -0,0 +1,68 @@
+<?php
+defined('CONTROL') or die('Acesso negado!');
+
+ // verifica se o formulário foi enviado
+ if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+    // verifica se o usuário e a password foram submetidas
+    $usuario = $_POST['usuario'] ?? null;
+    $senha = $_POST['senha'] ?? null;
+    $erro = null;
+
+    if(empty($usuario) || empty($senha)){
+        $erro = "usuário e senha são obrigatorios!";
+    }
+
+    // verifica se o usuário e password são válidos
+    if(empty($erro)){
+
+        $usuario = require_once __DIR__ . '/../inc/usuario.php';
+        
+        foreach($usuario as $user){
+            if($user['usuario'] == $usuario && password_verify($senha, $user['senha'])){
+
+                // efetua o login
+                $_SESSION['usuario'] = $usuario;
+
+                // volta para a página inicial
+                header('location: index.php?rota=home');
+            }
+        }
+
+        $erro = "Usuário e/ou senha inválidos!";
+    }
+
+ }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    
+    <form action="index.php?rota=login" method="post">
+        <h3>Login</h3>
+        <div>
+            <label for="usuario">Usuário:</label>
+            <input type="text" name="usuario" id="usuario">
+        </div>
+        <div>
+            <label for="senha">Senha:</label>
+            <input type="password" name="senha" id="senha">
+        </div>
+        <div>
+            <button type="submit">Entrar</button>
+        </div>
+    </form>
+
+    <?php if(!empty($erro)): ?>
+        <p style="color: red"><?= $erro ?></p>
+    <?php endif; ?>
+
+</body>
+</html>
